9a3a2d3c74198b4d37655bb8beae39f90ab0acd1
AmauryEsparza <amauryesparza@gmail.com>
2015-06-25 13:05:53 -0500

    Animating elements

--- app/src/main/java/com/example/amauryesparza/tweenanimation/MainActivity.java
+++ app/src/main/java/com/example/amauryesparza/tweenanimation/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.amauryesparza.tweenanimation;
 
 import android.app.Activity;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -21,7 +18,7 @@ public class MainActivity extends Activity implements Animation.AnimationListene
     private Animation scaleAnimation;
     private Animation translateAnimation;
     private Animation alphaAnimation;
-
+    private Animation rotateAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,26 +53,26 @@ public class MainActivity extends Activity implements Animation.AnimationListene
     }
 
     public void rotateAnimation(View v){
-        Animation rotateAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate_anim);
+        rotateAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate_anim);
         rotateAnimation.setAnimationListener(this);
         image.startAnimation(rotateAnimation);
 
     }
 
     public void scaleAnimation(View v){
-        Animation scaleAnimation = AnimationUtils.loadAnimation(this, R.anim.scale_anim);
+        scaleAnimation = AnimationUtils.loadAnimation(this, R.anim.scale_anim);
         scaleAnimation.setAnimationListener(this);
         image.startAnimation(scaleAnimation);
     }
 
     public void translateAnimation(View v){
-        Animation translateAnimation = AnimationUtils.loadAnimation(this, R.anim.tranlate_anim);
+        translateAnimation = AnimationUtils.loadAnimation(this, R.anim.tranlate_anim);
         translateAnimation.setAnimationListener(this);
         image.startAnimation(translateAnimation);
     }
 
     public void alphaAnimation(View v){
-        Animation alphaAnimation = AnimationUtils.loadAnimation(this, R.anim.alpha_anim);
+        alphaAnimation = AnimationUtils.loadAnimation(this, R.anim.alpha_anim);
         alphaAnimation.setAnimationListener(this);
         image.startAnimation(alphaAnimation);
 
